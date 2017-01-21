7f23f5505eea00bd6ce11a65cb904090c0d5f2ea
Ândrei d`Oliveira Mesquita Schuch <andreirs@outlook.com>
2017-01-21 01:14:47 -0200

    Asteroids Spawn System + Asteroid Behaviour

---
+++ Assets/Scripts/AsteroidBehaviour.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class AsteroidBehaviour : MonoBehaviour
+{
+    private new Rigidbody rigidbody;
+
+    private void Awake()
+    {
+        rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        rigidbody.velocity = Vector3.down * SpawnManager.fallingVelocity;
+    }
+
+    private void OnCollisionEnter(Collision other)
+    {
+        //TODO: shows up animation of the object being destroyed
+
+        Destroy(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,70 @@
+﻿using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class SpawnManager : MonoBehaviour
+{
+    public static float fallingVelocity = 12f;
+
+    [SerializeField]
+    private GameObject asteroidPrefab;
+
+    [Header("Position"), SerializeField, Range(0, 4)]
+    private float spawnXSpacement;
+
+    [Header("Spawn Configurations")]
+    [SerializeField, Range(0, 5)]
+    private float spawnDelay = 1;
+
+    [SerializeField, Range(.05f, 2f)]
+    private float minAsteroidScale, maxAsteroidScale;
+
+    private float lastGameSpawnTime = 0;
+
+    private void Awake()
+    {
+        if (!asteroidPrefab)
+            Debug.LogError("'asteroidPrefab' not assigned!", this);
+    }
+
+    private void OnEnable()
+    {
+        lastGameSpawnTime = Time.time;
+    }
+
+    private void Update()
+    {
+        if (Time.time - lastGameSpawnTime >= spawnDelay)
+        {
+            GameObject newAsteroid = SpawnNewAsteroid();
+
+            //Randomizes the asteroid's position
+            newAsteroid.transform.position = transform.position + Vector3.right * Random.Range(-spawnXSpacement, spawnXSpacement);
+
+            //Randomizes the asteroid's scale
+            newAsteroid.transform.localScale = Vector3.one * Random.Range(minAsteroidScale, maxAsteroidScale);
+
+            //Randomizes the asteroid's rotation
+            newAsteroid.transform.localRotation = 
+                Quaternion.Euler(
+                    Random.Range(-180, 180), 
+                    Random.Range(-180, 180), 
+                    Random.Range(-180, 180));
+
+            lastGameSpawnTime = Time.time;
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+
+        Gizmos.DrawLine(transform.position + -Vector3.right * spawnXSpacement,
+                        transform.position + Vector3.right * spawnXSpacement);
+    }
+
+    private GameObject SpawnNewAsteroid()
+    {
+        //TODO: Implement a descent spawn system
+        return Instantiate(asteroidPrefab);
+    }
+}

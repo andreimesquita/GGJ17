e6e93f894a569a3672b252f0d6fbc08715c97f82
Guilherme Quadros da Silva <guiquadros35@gmail.com>
2017-01-21 15:25:41 -0200

    fix spaceships velocity and rotation

--- Assets/Scripts/SpaceshipMove.cs
+++ Assets/Scripts/SpaceshipMove.cs
@@ -7,27 +7,38 @@ public class SpaceshipMove : MonoBehaviour
     [SerializeField] private float maximumVelocity;
     [SerializeField] private float minimumVelocity;
     [SerializeField] private float accelerationRate;
-    [SerializeField] private float maximumSteeringAngle;
-    [SerializeField] private float direction;
     [SerializeField] private float steeringDumping;
+    [SerializeField] private KeyCode keyCode;
+    [SerializeField] private float direction;
+
+    private float maximumSteeringAngle;
+    
 
     private Vector2 directionVector;
     private Quaternion maximumSteeringQuaternion;
     private float currentVelocity;
 
-    public void Awake()
+    private void Awake()
     {
         this.maximumVelocity = 6.0f;
         this.minimumVelocity = 1.0f;
         this.accelerationRate = 8.0f;
-        this.maximumSteeringAngle = 10.0f;
-        this.steeringDumping = 0.3f;
+        this.maximumSteeringAngle = 60.0f;
+        this.steeringDumping = 0.25f;
 
         this.directionVector = new Vector2(direction, 0.0f);
         this.maximumSteeringQuaternion = Quaternion.AngleAxis(maximumSteeringAngle, Vector3.forward);
     }
 
-    public void FixedUpdate()
+    private void Update()
+    {
+        if (Input.GetKeyDown(keyCode))
+        {
+            ToggleDirection();
+        }
+    }
+
+    private void FixedUpdate()
     {
         float velocityRatio = currentVelocity / maximumVelocity;
         float velocityCurveEvaluation = accelerationCurve.Evaluate(velocityRatio);
@@ -40,8 +51,8 @@ public class SpaceshipMove : MonoBehaviour
         this.currentVelocity = Mathf.Clamp(deltaVelocity, minimumVelocity, maximumVelocity);
 
         Quaternion lastRotation = this.transform.rotation;
-        Quaternion deltaRotation = Quaternion.Lerp(lastRotation, maximumSteeringQuaternion, velocityRatio * steeringDumping);
-
+        Quaternion deltaRotation = Quaternion.Slerp(lastRotation, maximumSteeringQuaternion, velocityRatio * steeringDumping);
+        
         this.transform.position = deltaPosition;
         this.transform.rotation = deltaRotation;
     }

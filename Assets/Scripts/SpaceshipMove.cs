85f42b42176d07dc09a6ced8062f1a013354eb4e
pabloparada <pablo.paradabol@gmail.com>
2017-01-22 14:57:17 -0200

    - Add presentation step when game starts.

--- Assets/Scripts/SpaceshipMove.cs
+++ Assets/Scripts/SpaceshipMove.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class SpaceshipMove : MonoBehaviour
@@ -11,21 +12,46 @@ public class SpaceshipMove : MonoBehaviour
     [SerializeField] private KeyCode keyCode;
     [SerializeField] private float direction;
     [SerializeField] private float maximumSteeringAngle;
+    [SerializeField] private Timer timer;
 
     private Vector2 directionVector;
     private Quaternion maximumSteeringQuaternion;
     private float currentVelocity;
-    private Timer timer;
+    private float defaultAccelerationRate;
+    private bool isPresentationStep;
+    private float presentationTime;
 
     private void Awake()
     {
         this.maximumVelocity = 6.0f;
         this.minimumVelocity = 1.0f;
-        this.accelerationRate = 8.0f;
+        this.defaultAccelerationRate = 8.0f;
+        this.accelerationRate = 0.0f;
         this.steeringDumping = 0.25f;
 
         this.directionVector = new Vector2(direction, 0.0f);
         this.maximumSteeringQuaternion = Quaternion.AngleAxis(maximumSteeringAngle, Vector3.forward);
+
+        this.presentationTime = 2.5f;
+
+        this.isPresentationStep = true;
+
+        timer.RegisterCallback(this.presentationTime, DisablePresentationStep);
+
+        StartCoroutine(AccelerateGracefully());
+    }
+
+    private void DisablePresentationStep()
+    {
+        this.isPresentationStep = !this.isPresentationStep;
+        this.accelerationRate = defaultAccelerationRate;
+    }
+
+    public IEnumerator AccelerateGracefully()
+    {
+        yield return new WaitForSeconds(this.presentationTime * 0.2f);
+
+        this.accelerationRate = Mathf.Clamp(defaultAccelerationRate * 0.005f, accelerationRate, defaultAccelerationRate);
     }
 
     private void FixedUpdate()
@@ -42,7 +68,6 @@ public class SpaceshipMove : MonoBehaviour
 
         Quaternion lastRotation = this.transform.rotation;
         Quaternion deltaRotation = Quaternion.Slerp(lastRotation, maximumSteeringQuaternion, velocityRatio * steeringDumping);
-
 
         Camera cam = Camera.main;
         float height = 2f * cam.orthographicSize;
@@ -63,6 +88,8 @@ public class SpaceshipMove : MonoBehaviour
 
     public void ReverseInitialDirection()
     {
+        if (isPresentationStep) return;
+
         this.directionVector = new Vector2(direction * -1.0f, 0.0f);
         this.maximumSteeringQuaternion = Quaternion.Inverse(maximumSteeringQuaternion);
         currentVelocity = minimumVelocity;
@@ -70,6 +97,8 @@ public class SpaceshipMove : MonoBehaviour
 
     public void ResetDirection()
     {
+        if (isPresentationStep) return;
+
         this.directionVector = new Vector2(direction, 0.0f);
         this.maximumSteeringQuaternion = Quaternion.AngleAxis(maximumSteeringAngle, Vector3.forward);
         currentVelocity = minimumVelocity;

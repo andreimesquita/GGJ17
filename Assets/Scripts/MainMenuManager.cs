d133f3c0ca39bbbfa308b5bdf0da597d3371ce5d
Ândrei d`Oliveira Mesquita Schuch <andreirs@outlook.com>
2017-01-22 09:21:41 -0200

    Main menu

---
+++ Assets/Scripts/MainMenuManager.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using Util;
+
+public class MainMenuManager : MonoBehaviour
+{
+    public void OnPlayPressed()
+    {
+        SceneManager.LoadScene(Constants.SCENE_ID_CHARACTER_SELECTOR);
+    }
+
+    public void OnAchievementsPressed()
+    {
+        //TODO: shows up the achievements' list
+    }
+
+    public void OnExitPressed()
+    {
+        Application.Quit();
+    }
+}

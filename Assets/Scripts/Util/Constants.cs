d0e2a38e9e20bbc70d2724a650e2cd532240061c
Ândrei d`Oliveira Mesquita Schuch <andreirs@outlook.com>
2017-01-22 08:54:18 -0200

    GGJ splashscreen

--- Assets/Scripts/Util/Constants.cs
+++ Assets/Scripts/Util/Constants.cs
@@ -5,8 +5,8 @@
         public static readonly string KEY_PLAYER_ONE_ID = "p1id";
         public static readonly string KEY_PLAYER_TWO_ID = "p2id";
 
-        public static readonly int SCENE_ID_MAIN_MENU = 1;
-        public static readonly int SCENE_ID_CHARACTER_SELECTOR = 2;
-        public static readonly int SCENE_ID_GAME = 3;
+        public static readonly int SCENE_ID_MAIN_MENU = 0;
+        public static readonly int SCENE_ID_CHARACTER_SELECTOR = 1;
+        public static readonly int SCENE_ID_GAME = 2;
     }
 }

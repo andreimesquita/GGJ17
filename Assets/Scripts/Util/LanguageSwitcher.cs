524c39d39c5fbe0074c5ad1231696ebf9c983ac8
andreirs <andreirs@outlook.com>
2017-01-29 20:30:14 -0200

    Added localization support to the Game and Missions' screens

---
+++ Assets/Scripts/Util/LanguageSwitcher.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using SmartLocalization;
+using UnityEngine;
+
+namespace Assets.Scripts.Util
+{
+    public class LanguageSwitcher : MonoBehaviour
+    {
+        public enum Language
+        {
+            Portuguese,
+            English
+        }
+
+        private static class LanguageHelper
+        {
+            public static string ParseLanguage(Language lang)
+            {
+                switch (lang)
+                {
+                    case Language.Portuguese:
+                        return "pt-BR";
+                        break;
+                    case Language.English:
+                        return "en";
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException("lang", lang, null);
+                }
+            }
+        }
+
+        public void SwitchLanguage(Language newLanguage)
+        {
+            string languageString = LanguageHelper.ParseLanguage(newLanguage);
+
+            LanguageManager.Instance.ChangeLanguage(languageString);
+        }
+
+#if ANDROID
+        private void Start()
+        {
+            Destroy(this.gameObject);
+        }
+#endif
+
+        private void OnGUI()
+        {
+            if (GUILayout.Button("English"))
+                SwitchLanguage(Language.English);
+
+            if (GUILayout.Button("Português"))
+                SwitchLanguage(Language.Portuguese);
+        }
+    }
+}
